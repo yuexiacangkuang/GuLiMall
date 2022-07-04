f473717c8d23598f43b78766bc6efd2b34a9d7b6
yuexia <againstfate@foxmail.com>
2022-07-04 09:06:52 +0800

    项目初始结构创建

---
+++ coupon/src/test/java/com/yuexia/gulimall/coupon/CouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuexia.gulimall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
