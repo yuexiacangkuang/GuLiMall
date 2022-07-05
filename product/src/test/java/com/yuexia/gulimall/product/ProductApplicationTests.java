d34fed4ea5bfcdb880b908fc66f4a68b54f982a2
yuexia <againstfate@foxmail.com>
2022-07-05 08:56:57 +0800

    项目初始结构创建

--- product/src/test/java/com/yuexia/gulimall/product/ProductApplicationTests.java
+++ product/src/test/java/com/yuexia/gulimall/product/ProductApplicationTests.java
@@ -1,13 +1,22 @@
 package com.yuexia.gulimall.product;
 
+import com.yuexia.gulimall.product.entity.BrandEntity;
+import com.yuexia.gulimall.product.service.BrandService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ProductApplicationTests {
 
+	@Autowired
+	BrandService brandService;
+
 	@Test
 	void contextLoads() {
+		BrandEntity brandEntity = new BrandEntity();
+		brandEntity.setName("特斯拉");
+		brandService.save(brandEntity);
 	}
 
 }
